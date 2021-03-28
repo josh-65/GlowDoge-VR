9706c664ab19a7b13e938e22867f24e3121d73f7
Josh-65 <joshuauk2004@gmail.com>
2021-03-28 20:55:23 +0100

    Dependency updates
    --Unity 2020.3.1f
    --SteamVR 2.7.3
    --HDRP 10.7
    --Mirror 32.6

---
+++ Assets/_Scripts/levelChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelChange : MonoBehaviour
+{
+    public int creditLength;
+
+    void Update() {
+        Scene S = SceneManager.GetActiveScene();
+        if (S.name == "M - Credits") {
+            StartCoroutine(reset());
+
+            IEnumerator reset() {
+                yield return new WaitForSeconds(creditLength);
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    public void Quit() {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+
+    public void loadCredits() {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/_Scripts/settings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class settings : MonoBehaviour {
+    
+    public AudioMixer mixer;
+
+    //Sets graphics
+    public void quality (int index) {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    //Sets volume
+    public void SetMaster(float master) {
+        mixer.SetFloat("master", master);
+    }
+    public void SetMusic(float music) {
+        mixer.SetFloat("music", music);
+    }
+    public void SetSFX(float sfx) {
+        mixer.SetFloat("sfx", sfx);
+    }
+    public void SetDialog(float dialog) {
+        mixer.SetFloat("dialog", dialog);
+    }
+    public void SetVoiceChat(float voiceChat) {
+        mixer.SetFloat("voiceChat", voiceChat);
+    }
+}
